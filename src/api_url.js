8c5ea100ebb733c16324dfd3605770b12a69586c
unknown <mprakhar35@gmail.com>
2023-02-01 23:50:40 +0530

    some changes

--- src/api_url.js
+++ src/api_url.js
@@ -1,5 +1,6 @@
-const BASE_URL = "https://sstonebats.com/api";
+const BASE_URL = "https://wallet-5klca.ondigitalocean.app/api";
 export default BASE_URL;
 
+//https://wallet-5klca.ondigitalocean.app/api/
 //http://64.227.128.113/api
 //https://sstonebats.com/api/
